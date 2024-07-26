f9349574ed0908ce95936fd5593c0f88b10c0434
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-07-26 23:52:36 +0530

    front page

--- app/src/main/java/com/example/edubridge/GetStarted.java
+++ app/src/main/java/com/example/edubridge/GetStarted.java
@@ -1,31 +1,21 @@
 package com.example.edubridge;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-AppCompatButton getBtn;
+public class GetStarted extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        getBtn=findViewById(R.id.getBtn);
-        getBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(MainActivity.this,GetStarted.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_get_started);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
