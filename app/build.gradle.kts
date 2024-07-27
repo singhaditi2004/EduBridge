37929ea4a1a94e71a784aebc2e7b2db2613f753e
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-07-27 23:58:36 +0530

    second page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,4 +43,5 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation ("com.intuit.sdp:sdp-android:1.1.1")
+    implementation ("com.google.android.material:material:1.5.0")
 }
