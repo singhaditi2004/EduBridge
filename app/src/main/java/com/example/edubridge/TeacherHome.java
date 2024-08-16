ee573b85a148e5d7735bcb6d8848c6f85ef704fe
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-08-17 00:14:45 +0530

    home page updatded

--- app/src/main/java/com/example/edubridge/TeacherHome.java
+++ app/src/main/java/com/example/edubridge/TeacherHome.java
@@ -1,12 +1,17 @@
 package com.example.edubridge;
 
 import android.os.Bundle;
-
+import android.os.Handler;
+import android.os.Looper;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class TeacherHome extends AppCompatActivity {
 
@@ -15,6 +20,11 @@ public class TeacherHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_teacher_home);
+        ViewPager2 viewPager2=findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        new TabLayoutMediator(tabLayout, viewPager2,
+                (tab, position) -> {}).attach();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
