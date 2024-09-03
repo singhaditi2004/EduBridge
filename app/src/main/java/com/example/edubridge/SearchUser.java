1d0a3886f77c1a934251fb943826f3fac8f41afb
Aditi Singh <aditisinghbais_cse21@ggct.co.in>
2024-09-04 00:05:04 +0530

    search user

--- app/src/main/java/com/example/edubridge/SearchUser.java
+++ app/src/main/java/com/example/edubridge/SearchUser.java
@@ -1,24 +1,60 @@
 package com.example.edubridge;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SearchUser extends AppCompatActivity {
-
+    RecyclerView searchUser;
+    ImageButton search,backButton;
+    EditText enterName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_search_user);
+        searchUser=findViewById(R.id.recycleUserSearch);
+        search=findViewById(R.id.searchBtUser);
+        enterName=findViewById(R.id.editTextSearchUser);
+        backButton=findViewById(R.id.backButt);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchItem=enterName.getText().toString();
+                if(searchItem.isEmpty() || searchItem.length()<3){
+                     enterName.setError("Invalid Username");
+                     return;
+                }
+                searchUserName(searchItem);
+            }
+        });
+        enterName.requestFocus();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void searchUserName(String searchName){
+        List<String> userNames=new ArrayList<>();
+
+
     }
 }
